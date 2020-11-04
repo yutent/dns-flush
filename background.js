09a2776dfa6ba4e87490354413d0ffc7d862eeed
宇天 <yutent.io@gmail.com>
2020-11-04 17:59:31 +0800

    1.0.0

--- background.js
+++ background.js
@@ -1,28 +1,27 @@
 /**
- *
- * @authors yutent (yutent@doui.cc)
- * @date    2018-07-07 20:59:35
- * @version $Id$
+ * DNS缓存杀手
+ * @author yutent<yutent.io@gmail.com>
+ * @date 2020/11/04 17:54:49
  */
 
 chrome.browserAction.onClicked.addListener(function(ctab) {
-  let cid = ctab.id
-  let tid = null
-  let code = `
-    let script = document.createElement('script')
+  var cid = ctab.id
+  var tid = null
+  var code = `
+    var script = document.createElement('script')
     script.textContent = \`
       chrome.send('clearHostResolverCache')
       chrome.send('flushSocketPools')
     \`
     document.body.appendChild(script)
   `
-  let callback = function() {
+  var callback = function() {
     chrome.tabs.remove(tid)
     chrome.tabs.executeScript(cid, { code: 'location.reload()' })
   }
 
   chrome.tabs.query({}, function(tabs) {
-    let done = false
+    var done = false
     for (let i = 0, tab; (tab = tabs[i++]); ) {
       if (/^chrome:\/\/net-internals/.test(tab.url)) {
         tid = tab.id
